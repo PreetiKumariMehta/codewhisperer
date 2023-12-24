6cd007137bf8e8630ed13f4531575ad909118f39
Ses Priye <kumari.preeti7777@gmail.com>
2023-12-24 23:42:55 +0530

    fix pipeline

--- lib/codewhisperer-stack.ts
+++ lib/codewhisperer-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+
 
 export class CodewhispererStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+// create an S3 bucket
+    const bucket = new cdk.aws_s3.Bucket(this, 'Bucket', {
+      versioned: true,
+      bucketName: 'codewhisperer-bucket',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // The code that defines your stack goes here
+    // create a vpc
+    const vpc = new cdk.aws_ec2.Vpc(this, 'Vpc', {
+      natGateways: 0,
+      vpcName: 'codewhisperer-vpc',
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'application',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          cidrMask: 28,
+          name: 'rds',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        }
+     ]
+   });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodewhispererQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    //Create an RDS Instance
+    const rdsdemo = new cdk.aws_rds.DatabaseInstance(this, 'Rds', {
+      engine: rds.DatabaseInstanceEngine.MYSQL,
+      vpc: vpc,
+      databaseName: 'codewhisperer',
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED}
+    });     
+}
+
 }
